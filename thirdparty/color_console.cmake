420b70751bc18c097dc2207e7356799938b30b0a
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-06-30 00:06:51 +0200

    added git tags and cleaned up

--- thirdparty/color_console.cmake
+++ thirdparty/color_console.cmake
@@ -1,9 +1,11 @@
 message("-- Fetching External Project: color-console")
 
-FetchContent_Declare(color-console
-	GIT_REPOSITORY https://github.com/imfl/color-console
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND ""
+FetchContent_Declare(
+  color-console
+  GIT_REPOSITORY    https://github.com/imfl/color-console
+  GIT_TAG           745f57141e6322e20cdda59a74ab1e00d107ade1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND     ""
 )
 
 FetchContent_GetProperties(color-console)

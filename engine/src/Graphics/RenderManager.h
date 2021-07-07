392374ce32c1b39736a3f784bdc1043b26a3e692
Christopher Gullberg Brady <37616589+KyaZero@users.noreply.github.com>
2021-07-07 15:21:23 +0200

    Removed Time  and prepared renderers for threading

--- engine/src/Graphics/RenderManager.h
+++ engine/src/Graphics/RenderManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Core/Constants.h"
 #include "RenderCommand.h"
 #include "SpriteRenderer.h"
 
@@ -14,15 +15,17 @@ namespace fw
         bool Init();
 
         void Submit(const RenderCommand& command);
-        void Render(f32 dt, f32 total_time);
+        void Render();
 
         Texture& GetRenderTexture();
         void Resize(u32 width, u32 height);
+
+        void Flip();
 
     private:
         std::unique_ptr<Texture> m_RenderTexture;
         Window& m_Window;
         SpriteRenderer m_SpriteRenderer;
-        std::vector<RenderCommand> m_RenderCommands;
+        std::vector<RenderCommand> m_RenderCommands[FRAME_COUNT];
     };
 }  // namespace fw

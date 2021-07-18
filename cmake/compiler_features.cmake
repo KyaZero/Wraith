6c615cadbc973bf298794733d9cbc2453ad77470
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-07-18 17:48:23 +0200

    update cmakelists to use c++23 to get c++latest in VS

--- cmake/compiler_features.cmake
+++ cmake/compiler_features.cmake
@@ -1,5 +1,7 @@
 if(NOT TARGET compiler_features)
     add_library(compiler_features INTERFACE)
 
-    target_compile_features(compiler_features INTERFACE cxx_std_20)
+    target_compile_features(compiler_features INTERFACE cxx_std_23)
 endif()
+
+set(CMAKE_CXX_STANDARD 23)

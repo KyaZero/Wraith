7e67c518142fa70dcf56bb910f900255a8ad007c
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-10-30 21:08:47 +0200

    add load/save support

--- editor/src/Panels/PanelManager.cpp
+++ editor/src/Panels/PanelManager.cpp
@@ -19,23 +19,40 @@ namespace Wraith
                     if (ImGui::MenuItem(panel->GetName(), nullptr, panel->IsVisible(), true))
                         panel->ToggleVisibility();
                 }
+
+                ImGui::Separator();
+
+                if (ImGui::BeginMenu("Editor Layout"))
+                {
+                    if (ImGui::MenuItem("Save Layout"))
+                    {
+                        const char* filter = "*.ini";
+                        auto path = tinyfd_saveFileDialog("ImGui Layout", "", 1, &filter, nullptr);
+                        if (path)
+                            ImGui::SaveIniSettingsToDisk(path);
+                    }
+                    if (ImGui::MenuItem("Load Layout"))
+                    {
+                        const char* filter = "*.ini";
+                        auto path = tinyfd_openFileDialog("Load Layout", "", 1, &filter, nullptr, 0);
+                        if (path)
+                            m_ImguiLayer.LoadSettings(path);
+                    }
+                    ImGui::EndMenu();
+                }
+
                 ImGui::EndMenu();
             }
+
             if (ImGui::BeginMenu("Help"))
             {
                 if (ImGui::MenuItem("Open Temp Directory"))
                 {
                     ShellExecuteW(0, L"open", TEMP_DIRECTORY.native().c_str(), 0, 0, SW_SHOWDEFAULT);
                 }
-                if (ImGui::MenuItem("Choose Layout"))
-                {
-                    const char* filter = "*.*";
-                    auto path = tinyfd_openFileDialog("ImGui Layout", "", 1, &filter, nullptr, 0);
-                    if (path)
-                        m_ImguiLayer.LoadSettings(path);
-                }
                 ImGui::EndMenu();
             }
+
             ImGui::EndMainMenuBar();
         }
 
